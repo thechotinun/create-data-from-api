ee8c58be168ab91b4806dad7b606df047a5e8eff
thechotinun <thechotinun@gmail.com>
2024-02-19 23:46:58 +0700

    fix transformData

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,16 @@ import { UserData, UserDataResponse, DepartmentSummary, departmentAgerang } from
 import { calculateAgeRange } from './utils/calculateAgeRange'
 
 function App() {
-  const [summary, setSummary] = useState<DepartmentSummary>({});
+  const initialSummary: DepartmentSummary[] = [];
+  const [summary, setSummary] = useState<DepartmentSummary[]>(initialSummary);
 
   const transformData =  useCallback(async (users: UserData[]) => {
-    const setSummary: DepartmentSummary = {};
+    const summaryData: DepartmentSummary = {};
     const departmentAgerang: departmentAgerang = {};
     const transformedDataPromises = users?.length && map(users, async (user) => {
         const { company, gender, age, hair, firstName, lastName, address } = user;
-        if (!setSummary[company.department]) {
-          setSummary[company.department] = {
+        if (!summaryData[company.department]) {
+          summaryData[company.department] = {
             male: 0,
             female: 0,
             ageRange: '',
@@ -28,16 +29,16 @@ function App() {
         } else {
             departmentAgerang[company.department].push(age);
         }
-        gender === 'male' && setSummary[company.department]['male']++;
-        gender === 'female' && setSummary[company.department]['female']++;
-        setSummary[company.department]['ageRange'] = await calculateAgeRange(departmentAgerang[company.department]);
-        setSummary[company.department].hair[hair.color] = (setSummary[company.department].hair[hair.color] || 0) + 1;
-        setSummary[company.department].addressUser[`${firstName}${lastName}`] = address.postalCode;
+        gender === 'male' && summaryData[company.department]['male']++;
+        gender === 'female' && summaryData[company.department]['female']++;
+        summaryData[company.department]['ageRange'] = await calculateAgeRange(departmentAgerang[company.department]);
+        summaryData[company.department].hair[hair.color] = (summaryData[company.department].hair[hair.color] || 0) + 1;
+        summaryData[company.department].addressUser[`${firstName}${lastName}`] = address.postalCode;
     });
-
+    
     await Promise.all(transformedDataPromises || []);
     
-    return setSummary;
+    return Object.keys(summaryData).map(key => ({ [key]: summaryData[key] }));
   },[]);
 
   useEffect(() => {
